6b45503be7a5d616377bce5fcfd2839680707b49
Alexander Bedyaev <hairedball@gmail.com>
2022-01-18 12:15:17 +0300

    Initial_Commit

---
+++ src/Character.java
@@ -0,0 +1,18 @@
+public class Character {
+
+    String name = null;
+    int health = 100;
+    double gold = 0;
+    int agility = 0;
+    int strength = 0;
+    int experiance = 0;
+
+
+
+
+    void attack () {
+
+    }
+
+
+}
